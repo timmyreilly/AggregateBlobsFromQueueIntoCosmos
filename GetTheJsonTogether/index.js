5229f7dba13b793f518b13af675e4e6032434618
timmyreilly <timreilly@live.com>
2018-09-11 14:35:50 -0700

    lets debug

--- GetTheJsonTogether/index.js
+++ GetTheJsonTogether/index.js
@@ -1,5 +1,18 @@
+const azure = require('azure-storage'); 
+const fs = require('fs') ;
+
 module.exports = async function (context, myQueueItem) {
+    context.log(process.version); 
+    context.log(process.env["QueueConnectionString"]); 
+    const blobService = azure.createBlobService(process.env["QueueConnectionString"]); 
+    
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
-    //const cheese = JSON.parse(myQueueItem); 
+    
     myQueueItem.forEach(x => context.log("Check it:" + x.content)); 
+
+    const myStuff = myQueueItem.forEach(x => x.content.split("/")); 
+    context.log(myStuff); 
+
+    var ourBlobs = myQueueItem.forEach(x => blobService.getBlobToText(x)); 
+//     blobService.getBlobToText()
 };
