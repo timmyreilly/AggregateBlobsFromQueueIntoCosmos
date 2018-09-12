ce9cf26a311478d9556d12e0ae1118c35fc0ff8b
timmyreilly <timreilly@live.com>
2018-09-11 17:33:24 -0700

    geeer

--- GetTheJsonTogether/index.js
+++ GetTheJsonTogether/index.js
@@ -1,25 +1,38 @@
-const azure = require('azure-storage'); 
-const fs = require('fs') ;
+const azure = require('azure-storage');
+const fs = require('fs');
 
 module.exports = async function (context, myQueueItem) {
-    context.log(process.version); 
-    context.log(process.env["QueueConnectionString"]); 
-    const blobService = azure.createBlobService(process.env["QueueConnectionString"]); 
-    
+    context.log(process.version);
+    context.log(process.env["QueueConnectionString"]);
+    const blobService = azure.createBlobService(process.env["BlobConnectionString"]);
+
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
-    
-    myQueueItem.forEach(x => context.log("Check it:" + x.content)); 
 
-    const myStuff = myQueueItem.forEach(x => x.content.split("/")); 
-    const m = myQueueItem.map( function(x) { return x.content.split("/")} ); 
+    myQueueItem.forEach(x => context.log("Check it:" + x.content));
 
-    let beer;  
-    const ourB = m.map( x =>  blobService.getBlobToText(x[1], x[2], beer,  function(error, result, response) {
-        if(!error) {
-            context.log(result); 
-        }
-    })); 
+    const myStuff = myQueueItem.forEach(x => x.content.split("/"));
+    const m = myQueueItem.map(function (x) { return x.content.split("/") });
+
+
+    m.forEach(x => 
+        blobService.getBlobToText(
+            x[1],
+            x[2],
+            function(err, blobContent, blob) {
+                if(err) {
+                    context.log("couldnt download: ", x[1], x[2]); 
+                    context.log("POOOOOP");
+                    context.log(err); 
+
+                } else {
+                    context.log(blobContent); 
+                }
+            }
+        )
+    )
+
     
+
     // var ourBlobs = myQueueItem.forEach(x => blobService.getBlobToText(x)); 
-//     blobService.getBlobToText()
+    //     blobService.getBlobToText()
 };
